7a7d0cf12a6697028cf1f138cbef53aff1df25b0
smiley0907 <pcr.bobbepalli@gmail.com>
2023-01-23 20:12:26 +0530

    Create App.java

---
+++ chennai/src/main/java/chennai/App.java
@@ -0,0 +1,13 @@
+package chennai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
